313617cc682c12f6e4e077734bd1e119e8590773
Arif <marifullaah@gmail.com>
2022-10-16 23:50:20 +0600

    check the som

--- src/text.js
+++ src/text.js
@@ -1,4 +1,1 @@
-const student = { name: 'fike', job: 'practice', address: 'janina' }
-const { address, ...rest } = student;
-console.log(address)
-console.log(rest)       
+const student = { name: 'fike' };
