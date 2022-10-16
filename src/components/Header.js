0e2bbeaa4bfa0b33bb48208549ab5d60e95ac582
Arif <marifullaah@gmail.com>
2022-10-16 23:50:20 +0600

    check the some issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,7 @@ const Header = () => {
     return (
         <div>
             <nav className="flex justify-around bg-black-700">
-
-                <h1 className="text-3xt"> Login and Register</h1>
+                <h1 className="text-3xl"> Login and Register</h1>
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
             </nav>
